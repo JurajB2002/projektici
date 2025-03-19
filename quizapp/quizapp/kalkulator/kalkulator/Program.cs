a77c1f04b83855fc7a23d5a4f044490452ba2555
JurajB2002 <168214904+JurajB2002@users.noreply.github.com>
2025-03-19 17:54:25 +0100

    Add files via upload

---
+++ quizapp/quizapp/kalkulator/kalkulator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Unesi dva broja");
+bool try1 = int.TryParse(Console.ReadLine(), out int num1);
+bool try2 = int.TryParse(Console.ReadLine(), out int num2);
+while (!try2 || !try1)
+{
+   Console.WriteLine("Buraz moras brojeve unjet");
+   try1 = int.TryParse(Console.ReadLine(), out  num1);
+   try2 = int.TryParse(Console.ReadLine(), out  num2);
+
+}
+Console.WriteLine("unesi operator (+, -, *, /)");
+char operat = char.Parse(Console.ReadLine());
+if (operat != '+' && operat != '-' && operat != '*' && operat != '/')
+{
+    Console.WriteLine("Sta radis Buraz sta neznas citat");
+}
+int res;
+if (operat == '+')
+{
+    res = num1 + num2;
+    Console.WriteLine($"Odabrao si {operat} pa {num1} {operat} {num2} je {res}");
+}
+else if (operat == '-')
+{
+    res = num1 - num2;
+    Console.WriteLine($"Odabrao si {operat} pa {num1} {operat} {num2} je {res}");
+}
+else if (operat == '*')
+{
+    res = num1 * num2;
+    Console.WriteLine($"Odabrao si {operat} pa {num1} {operat} {num2} je {res}");
+}
+else if (operat == '/')
+{
+    if (num1 == 0 || num2 == 0)
+    {
+        Console.WriteLine("Buraz nemozes djelit s nulom");
+    }
+    else
+    {
+        double ResHole = (double)num1 / num2;
+        Console.WriteLine($"Odabrao si {operat} pa {num1} {operat} {num2} je {ResHole}");
+    }
+   
+}
+
+Console.ReadKey();
